5fe6226af93b9e32bc7c13e34d41f1a0552bf0e4
marcosfitzsimons <marcosfitzsimons@gmail.com>
2022-11-10 15:38:17 -0300

    Add min height to the loading page

--- app/(index)/loading.jsx
+++ app/(index)/loading.jsx
@@ -2,7 +2,7 @@ import { BiLoaderAlt } from "react-icons/bi";
 
 const Loading = () => {
   return (
-    <div className="w-[min(85%,1200px)] mx-auto pt-24 lg:pt-32 flex justify-center">
+    <div className="w-[min(85%,1200px)] mx-auto pt-24 lg:pt-32 flex justify-center min-h-screen">
       <BiLoaderAlt className="animate-spin text-3xl text-yellow-400" />
     </div>
   );
